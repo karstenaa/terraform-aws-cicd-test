a535262196f727331dbaab97e88fceb0db5816d7
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-08-16 17:09:10 +0700

    update notif-github.py

--- notif-github.py
+++ notif-github.py
@@ -4,10 +4,8 @@ import os
 
 command = os.popen('terraform show tfplan -no-color')
 tf_plan = command.read()
-print tf_plan
 command.close()
 
 git_token = sys.argv[1]
 headers = {"Authorization": "token " + git_token}
-r = requests.post('https://api.github.com/repos/traveloka/terraform-aws-cicd-test/issues/1/comments', headers=headers, json={"body": tf_plan})
-print r.text
+r = requests.post('https://api.github.com/repos/traveloka/terraform-aws-cicd-test/issues/1/comments', headers=headers, json={"body": "```hcl\n" + tf_plan}) 
