58d551bb79bc7ca623b3a76ee1fda7b52e3fad0f
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-08-23 14:54:17 +0700

    Get git token from env var instead of arg

--- notif-github.py
+++ notif-github.py
@@ -6,6 +6,6 @@ command = os.popen('terraform show tfplan -no-color')
 tf_plan = command.read()
 command.close()
 
-git_token = sys.argv[1]
+git_token = os.environ["GIT_TOKEN"]
 headers = {"Authorization": "token " + git_token}
-r = requests.post('https://api.github.com/repos/traveloka/terraform-aws-cicd-test/issues/1/comments', headers=headers, json={"body": "```hcl\n" + tf_plan}) 
+r = requests.post('https://api.github.com/repos/traveloka/terraform-aws-cicd-test/issues/1/comments', headers=headers, json={"body": "```hcl\n" + tf_plan})
