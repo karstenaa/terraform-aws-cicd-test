5010e888c0c77717a0fa9895b3a0f6e5cf722ae5
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-08-24 17:04:33 +0700

    test fix

--- notif-github.py
+++ notif-github.py
@@ -7,9 +7,10 @@ tf_plan = command.read()
 command.close()
 
 git_token = os.environ["GIT_TOKEN"]
-tf_working_dir = os.environ["TF_WORKING_DIR"]
 f = open("build/metadata.json", "r")
 metadata = f.read()
 
 headers = {"Authorization": "token " + git_token}
-r = requests.post('https://api.github.com/repos/traveloka/terraform-aws-cicd-test/issues/1/comments', headers=headers, json={"body": "```json\n" + metadata + "```\n```hcl\n" + tf_plan +"\n"})
+json = {"body": "```json\n" + metadata + "```\n```hcl\n" + tf_plan +"```\n"}
+print json
+r = requests.post('https://api.github.com/repos/traveloka/terraform-aws-cicd-test/issues/1/comments', headers=headers, json=json)
