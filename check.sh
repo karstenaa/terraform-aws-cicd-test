21396a81a167311ce20b8c33e74dbd3320871d70
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-08-24 11:43:09 +0700

    Add checking

--- check.sh
+++ check.sh
@@ -1,1 +1,13 @@
-git diff origin/master --name-only | grep "\.tf$" | sed 's/\/[^/]\+\.tf$//g' | uniq
+git merge-base --is-ancestor origin/master HEAD
+if [ $? -ne 0 ]; then
+    echo "Error: Commit isn't based on origin/master"
+    exit 1
+fi
+result=$(git diff origin/master --name-only | grep "\.tf$" | sed 's/\/[^/]\+\.tf$//g' | uniq)
+echo "Folders contain tf file changes:
+$result
+"
+if [[ $(echo $result | wc -l) -gt 1 ]]; then
+    echo "Error: Multiple folder contain tf file changes"
+    exit 1
+fi
