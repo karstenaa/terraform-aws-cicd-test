2286b478f5d2bc4e3ef69efcea3e303bcd51e83c
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-08-24 16:09:07 +0700

    add metadata to notification

--- notif-github.py
+++ notif-github.py
@@ -8,5 +8,8 @@ command.close()
 
 git_token = os.environ["GIT_TOKEN"]
 tf_working_dir = os.environ["TF_WORKING_DIR"]
+f = open("build/", "r")
+metadata = f.read()
+
 headers = {"Authorization": "token " + git_token}
-r = requests.post('https://api.github.com/repos/traveloka/terraform-aws-cicd-test/issues/1/comments', headers=headers, json={"body": "TF_WORKING_DIR = " + tf_working_dir + "\n" + "```hcl\n" + tf_plan})
+r = requests.post('https://api.github.com/repos/traveloka/terraform-aws-cicd-test/issues/1/comments', headers=headers, json={"body": "```json\n" + metadata + "```\n```hcl\n" + tf_plan +"\n"})
