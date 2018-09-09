ef913f6b566675ada54771b82f3a23c6e0b55112
karsten.agathon <karsten.agathon@traveloka.com>
2018-09-10 05:28:26 +0700

    ASD

--- check.sh
+++ check.sh
@@ -1,10 +1,10 @@
-git merge-base --is-ancestor origin/master HEAD
+git merge-base --is-ancestor upstream/master HEAD
 if [ $? -ne 0 ]; then
     echo "Error: Commit isn't based on origin/master"
     exit 1
 fi
 
-if [ $GIT_MASTER_COMMIT != $LATEST_COMMIT_APPLY ]; then
+if [ $GIT_MASTER_COMMIT_ID != $LATEST_COMMIT_APPLY ]; then
     echo "Error: Git commit on origin/master($LATEST_COMMIT_APPLY) and latest-commit-apply($LATEST_COMMIT_APPLY) on S3 aren't equal"
 fi
 
