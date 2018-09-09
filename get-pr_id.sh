d8f0a573abbe8e5fbbdae671a0844a924dd95256
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-09-10 01:17:12 +0700

    fix (#7)
    * fix
    
    * asd

--- get-pr_id.sh
+++ get-pr_id.sh
@@ -1,3 +1,3 @@
-curl -H "Authorization: token $GIT_TOKEN" -X GET https://api.github.com/search/issues?q=${GIT_COMMIT_ID}+is:merged
-export PR_ID="$(curl -H \"Authorization: token $GIT_TOKEN\" -X GET https://api.github.com/search/issues?q=${GIT_COMMIT_ID}+is:merged | jq -r '.items[0].number')"
+curl -H "Authorization: token $GITHUB_TOKEN" -X GET https://api.github.com/search/issues?q=${GIT_COMMIT_ID}+is:merged
+export PR_ID="$(curl -H \"Authorization: token $GITHUB_TOKEN\" -X GET https://api.github.com/search/issues?q=${GIT_COMMIT_ID}+is:merged | jq -r '.items[0].number')"
 echo "PR_ID=$PR_ID"
