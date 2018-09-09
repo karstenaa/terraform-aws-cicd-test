afbfffb8f662c300604b56ed5098aff0db5f98a1
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-09-10 04:57:59 +0700

    ASD (#10)
    * ASD
    
    * ASD

--- check.sh
+++ check.sh
@@ -3,6 +3,11 @@ if [ $? -ne 0 ]; then
     echo "Error: Commit isn't based on origin/master"
     exit 1
 fi
+
+if [ $GIT_MASTER_COMMIT -ne $LATEST_COMMIT_APPLY ]; then
+    echo "Error: Git commit on origin/master($LATEST_COMMIT_APPLY) and latest-commit-apply($LATEST_COMMIT_APPLY) on S3 aren't equal"
+fi
+
 export TF_WORKING_DIR="$(git diff origin/master --name-only | grep '\.tf$' | sed 's/\/[^/]\+\.tf$//g' | uniq)"
 echo "Folders contain tf file changes:
 $TF_WORKING_DIR
