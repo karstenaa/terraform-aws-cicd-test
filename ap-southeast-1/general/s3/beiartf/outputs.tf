d7c07f73a2575f0a9ce068ef00f94a38430694f0
karsten.agathon <karsten.agathon@traveloka.com>
2018-09-10 11:51:28 +0700

    ASD

--- ap-southeast-1/general/s3/beiartf/outputs.tf
+++ ap-southeast-1/general/s3/beiartf/outputs.tf
@@ -1,5 +1,5 @@
 
 output "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket "
+  description = "The ARN of the S3 bucket   "
   value       = "${module.beiartf_s3.s3_bucket_arn}"
 }
--- notif-github.py
+++ notif-github.py
@@ -12,7 +12,5 @@ f = open("build/metadata.json", "r")
 metadata = f.read()
 headers = {"Authorization": "token " + git_token}
 json = {"body": "```json\n" + metadata + "```\n```hcl\n" + tf_plan +"```\n"}
-print pr_id
 r = requests.post('https://api.github.com/repos/traveloka/terraform-aws-cicd-test/issues/' + pr_id +'/comments', headers=headers, json=json)
 print r.json
-print r.text
