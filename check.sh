30f0730a0b5ae10b487341ced0604befe38999d8
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-09-10 12:00:19 +0700

    ASD (#12)
    * ASD
    
    * ASD
    
    * ASD
    
    * ASD
    
    * ASD
    
    * ASD
    
    * ASD
    
    * ASD
    
    * ASD
    
    * ASD
    
    * ASD

--- check.sh
+++ check.sh
@@ -4,8 +4,9 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-if [ $GIT_MASTER_COMMIT -ne $LATEST_COMMIT_APPLY ]; then
-    echo "Error: Git commit on origin/master($LATEST_COMMIT_APPLY) and latest-commit-apply($LATEST_COMMIT_APPLY) on S3 aren't equal"
+if [ $GIT_MASTER_COMMIT_ID != $LATEST_COMMIT_APPLY ]; then
+    echo "Error: Git commit on origin/master($GIT_MASTER_COMMIT_ID) and latest-commit-apply($LATEST_COMMIT_APPLY) on S3 aren't equal"
+    exit 1
 fi
 
 export TF_WORKING_DIR="$(git diff origin/master --name-only | grep '\.tf$' | sed 's/\/[^/]\+\.tf$//g' | uniq)"
--- notif-github.py
+++ notif-github.py
@@ -2,7 +2,7 @@ import sys
 import requests
 import os
 
-command = os.popen('terraform show build/tfplan -no-color')
+command = os.popen('terraform show build/terraform.tfplan -no-color')
 tf_plan = command.read()
 command.close()
 
@@ -12,7 +12,5 @@ f = open("build/metadata.json", "r")
 metadata = f.read()
 headers = {"Authorization": "token " + git_token}
 json = {"body": "```json\n" + metadata + "```\n```hcl\n" + tf_plan +"```\n"}
-print pr_id
 r = requests.post('https://api.github.com/repos/traveloka/terraform-aws-cicd-test/issues/' + pr_id +'/comments', headers=headers, json=json)
 print r.json
-print r.text
