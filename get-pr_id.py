f02ecc886ff74014525fec20fb6fc5ee8629d89c
karsten.agathon <karsten.agathon@traveloka.com>
2018-09-10 11:15:46 +0700

    ASD

--- get-pr_id.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-git_token = os.environ["GIT_TOKEN"]
-git_commit_id = os.environ["GIT_COMMIT_ID"]
-headers = {"Authorization": "token " + git_token}
-payload = {"q": git_commit_id + "+is:merged"}
-r = requests.get('https://api.github.com/search/issues', headers=headers, params=payload)
-print r.text
