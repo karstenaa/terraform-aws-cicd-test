597f09492cafc48a69e292200f59c3acbc1568f5
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-09-19 12:38:01 +0700

    Remove role id (#21)

--- ap-southeast-1/general/s3/tv-sql-product/main.tf
+++ ap-southeast-1/general/s3/tv-sql-product/main.tf
@@ -1,8 +1,8 @@
-
 provider "aws" {
   version = "1.13.0"
   region  = "ap-southeast-1"
 }
+
 terraform {
   backend "s3" {
     bucket         = "default-terraform-state-cicd-ap-southeast-1-170466898939"
@@ -11,6 +11,7 @@ terraform {
     dynamodb_table = "default-terraform-state-cicd-ap-southeast-1-170466898939"
   }
 }
+
 module "tv_sql_product_s3_bucket_name" {
   source = "github.com/traveloka/terraform-aws-resource-naming?ref=v0.4.0"
 
@@ -39,6 +40,7 @@ resource "aws_s3_bucket_policy" "tv_sql_product" {
   bucket = "${aws_s3_bucket.tv_sql_product.id}"
   policy = "${data.aws_iam_policy_document.tv_sql_product.json}"
 }
+
 module "beiartf_s3" {
   source = "github.com/traveloka/terraform-aws-beiartf-s3"
 
@@ -48,7 +50,6 @@ module "beiartf_s3" {
     "AROAJNIF36KKKUH3LUBOC:*", # role beiartf ci writer
     "AROAJZR75IFQBR2RADMG6:*", # role beiartf reader
     "AROAI2IEV4NMLKC7UPSO4:*", # role beiartf Admin
-    "AROAJTJVOI3G5TVVM2M76:*", # role beiartf writer for data team
     "AROAIKBSFYLAN6UXFHTKS:*", # role beiartf bei
   ]
 }
