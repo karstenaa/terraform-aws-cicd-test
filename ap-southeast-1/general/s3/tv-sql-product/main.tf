1c1851e2328056969027a2a2dbf7ab9b1f379631
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-09-26 05:54:27 +0700

    Test refactor script ci (#25)

--- ap-southeast-1/general/s3/tv-sql-product/main.tf
+++ ap-southeast-1/general/s3/tv-sql-product/main.tf
@@ -47,9 +47,9 @@ module "beiartf_s3" {
   environment = "management"
 
   roles_ids = [
-    "AROAJNIF36KKKUH3LUBOC:*", # role beiartf ci writer
-    "AROAJZR75IFQBR2RADMG6:*", # role beiartf reader
-    "AROAI2IEV4NMLKC7UPSO4:*", # role beiartf Admin
-    "AROAIKBSFYLAN6UXFHTKS:*", # role beiartf bei
+    "AROAJNIF36KKKUH3LUBOC:*",    # role beiartf ci writer
+    "AROAJZR75IFQBR2RADMG6:*",    # role beiartf reader
+    "AROAI2IEV4NMLKC7UPSO4:*",    # role beiartf Admin
+    "AROAIKBSFYLAN6UXFHTKSasd:*", # role beiartf bei
   ]
 }
