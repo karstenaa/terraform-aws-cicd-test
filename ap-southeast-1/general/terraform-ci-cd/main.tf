8e0443a49d7f2969c78ea0cd631396d439fe4113
karsten.agathon <karsten.agathon@traveloka.com>
2018-11-05 16:38:54 +0700

    test

--- ap-southeast-1/general/terraform-ci-cd/main.tf
+++ ap-southeast-1/general/terraform-ci-cd/main.tf
@@ -4,15 +4,15 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "default-terraform-state-cicd-ap-southeast-1-170466898939"
+    bucket         = "default-terraform-state-cicd-test-ap-southeast-1-517530806209"
     key            = "ap-southeast-1/general/terraform-ci-cd/terraform.tfstate"
     region         = "ap-southeast-1"
-    dynamodb_table = "default-terraform-state-cicd-ap-southeast-1-170466898939"
+    dynamodb_table = "default-terraform-state-cicd-test-ap-southeast-1-517530806209"
   }
 }
 
 module "terraform_aws_codebuild" {
-  source            = "github.com/traveloka/terraform-aws-codebuild-terraform-ci-cd.git?ref=v0.1.0"
+  source            = "github.com/traveloka/terraform-aws-codebuild-terraform-ci-cd.git?ref=v0.1.4"
   image             = "traveloka/codebuild-terraform-ci-cd-image:latest"
   terraform_version = "0.11.8"
   product_domain    = "bei"
